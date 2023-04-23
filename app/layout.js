6c8dc0bb549163dfc1608adc242d45469c948e21
Jackie Wong <wongzjackie@gmail.com>
2023-04-23 00:25:46 -1000

    Update layout.js
    - Add CircularStd font

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,23 @@
 import './globals.css';
+import localFont from 'next/font/local';
 import { Inter } from 'next/font/google';
 
 const inter = Inter({ subsets: ['latin'] });
+
+const circular = localFont({
+  src: [
+    {
+      path: './fonts/CircularStd-Black.woff2',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: './font/CircularStd-Bold.woff2',
+      weight: '700',
+      style: 'normal',
+    },
+  ],
+});
 
 export const metadata = {
   title: 'Spotify Profile',
@@ -12,7 +28,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang='en'>
-      <body className={`${inter.className}`}>{children}</body>
+      <body className={`${circular.className}`}>{children}</body>
     </html>
   );
 }
