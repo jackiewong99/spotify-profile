738ce8b9a641b84954f4f695c03b8962346a149f
Jackie Wong <wongzjackie@gmail.com>
2023-04-25 01:02:25 -1000

    Update page.js
    - Update font and text styling of the root page.js layout.
    - Add hover animation to the log in button.

--- app/page.js
+++ app/page.js
@@ -1,12 +1,12 @@
 const Home = () => {
   return (
     <main className='flex min-h-screen flex-col items-center justify-center p-24 text-white-100 bg-gray-800'>
-      <h1 className='text-3xl mb-7'>Spotify Profile</h1>
+      <h1 className='mb-7 text-4xl font-black'>Spotify Profile</h1>
       <a
         href='/'
-        className='text-2xl font-bold bg-green-500 px-16 py-4 rounded-full hover:bg-green-600 hover:text-white-200'
+        className='text-base font-normal tracking-[0.15em] bg-green-500 px-10 py-4 rounded-full transition-all hover:bg-green-600 hover:text-white-200 hover:duration-300'
       >
-        Log In To Spotify
+        LOG IN TO SPOTIFY
       </a>
     </main>
   );
