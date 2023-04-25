eb63bf1522dc7742d899e5f567a08ab49e42bc55
Jackie Wong <wongzjackie@gmail.com>
2023-04-24 17:18:22 -1000

    Update page.js
    - Update UI of root page.

--- app/page.js
+++ app/page.js
@@ -1,7 +1,13 @@
 const Home = () => {
   return (
-    <main className='flex min-h-screen flex-col items-center justify-between p-24 text-white bg-gray-800'>
-      <h1 className='text-3xl'>Spotify Profile</h1>
+    <main className='flex min-h-screen flex-col items-center justify-center p-24 text-white-100 bg-gray-800'>
+      <h1 className='text-3xl mb-7'>Spotify Profile</h1>
+      <a
+        href='/'
+        className='text-2xl font-bold bg-green-500 px-16 py-4 rounded-full hover:bg-green-600 hover:text-white-200'
+      >
+        Log In To Spotify
+      </a>
     </main>
   );
 };
