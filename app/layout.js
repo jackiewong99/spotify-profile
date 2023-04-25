c880535918a77c343bf7d8e92585cff773c615ce
Jackie Wong <wongzjackie@gmail.com>
2023-04-25 01:01:34 -1000

    Update layout.js
    - Delete boilerplate font import and usage.
    - Delete previous local import of Circular Std font family.

--- app/layout.js
+++ app/layout.js
@@ -1,23 +1,4 @@
 import './globals.css';
-import localFont from 'next/font/local';
-import { Inter } from 'next/font/google';
-
-const inter = Inter({ subsets: ['latin'] });
-
-const circular = localFont({
-  src: [
-    {
-      path: './fonts/CircularStd-Black.woff2',
-      weight: '400',
-      style: 'normal',
-    },
-    {
-      path: './fonts/CircularStd-Bold.woff2',
-      weight: '700',
-      style: 'normal',
-    },
-  ],
-});
 
 export const metadata = {
   title: 'Spotify Profile',
@@ -28,7 +9,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang='en'>
-      <body className={`${circular.className}`}>{children}</body>
+      <body>{children}</body>
     </html>
   );
 }
