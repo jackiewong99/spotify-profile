f294da5a945c3c7214aaf08c0f5c254dfaa4ddb0
Jackie Wong <wongzjackie@gmail.com>
2023-05-08 20:46:31 -1000

    Update root page.js
    - Update JSX structure for improved alignment of elements.
    - Lower font weight of h1 header (from font-black to font-bold).

--- app/page.js
+++ app/page.js
@@ -1,13 +1,17 @@
+import Link from 'next/link';
+
 const Home = () => {
   return (
-    <main className='flex min-h-screen flex-col items-center justify-center p-24 text-white-100 bg-gray-800'>
-      <h1 className='mb-7 text-4xl font-black'>Spotify Profile</h1>
-      <a
-        href='/'
-        className='text-base font-normal tracking-[0.15em] bg-green-600 px-10 py-4 rounded-full transition-all hover:bg-green-700 hover:text-white-200 hover:duration-300'
-      >
-        LOG IN TO SPOTIFY
-      </a>
+    <main className='flex min-h-screen flex-col items-center justify-center text-white-100 bg-gray-800'>
+      <div className='flex flex-col items-center'>
+        <h1 className='mb-7 text-4xl font-bold'>Spotify Profile</h1>
+        <Link
+          href='/api/spotify_login'
+          className='px-10 py-4 rounded-full text-base font-normal tracking-[0.15em] bg-green-600 transition-all hover:bg-green-700 hover:text-white-200 hover:duration-300'
+        >
+          LOG IN TO SPOTIFY
+        </Link>
+      </div>
     </main>
   );
 };
