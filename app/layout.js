9194a482573f2a6a7dfb44cda7ea58554e109670
Jackie Wong <wongzjackie@gmail.com>
2023-11-02 21:11:14 -1000

    Update layout.js
    - Use server session and SessionProvider component to pass state of session to children components.
    -Add Sidenav component to root layout.

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,8 @@
 import './globals.css';
+import { getServerSession } from 'next-auth';
+// Components
+import SessionProvider from './components/SessionProvider';
+import Sidenav from './components/Sidenav';
 
 export const metadata = {
   title: 'Spotify Profile',
@@ -6,10 +10,18 @@ export const metadata = {
     'A simple web app built with Next.js that allows users to view their Spotify profile.',
 };
 
-export default function RootLayout({ children }) {
+export default async function RootLayout({ children }) {
+  const session = await getServerSession();
   return (
     <html lang='en'>
-      <body>{children}</body>
+      <body className='min-h-screen'>
+        <SessionProvider session={session} refetchInterval={1800}>
+          <div className='flex justify-start'>
+            <Sidenav />
+            {children}
+          </div>
+        </SessionProvider>
+      </body>
     </html>
   );
 }
