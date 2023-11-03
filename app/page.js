813531bffc963e8cf3cb5b371f021588ea5c79ef
Jackie Wong <wongzjackie@gmail.com>
2023-11-02 21:13:26 -1000

    Update root page.js.
    - Add LoginPrompt and Profile component to root page.
    - Conditionally render login and profile based on authorization status.
    - Use data from useSession hook to perform conditional rendering.

--- app/page.js
+++ app/page.js
@@ -1,18 +1,15 @@
-import Link from 'next/link';
+'use client';
+import { useSession } from 'next-auth/react';
+// Components
+import LoginPrompt from './components/LoginPrompt';
+import Profile from './components/Profile';
 
 const Home = () => {
+  const { data } = useSession();
   return (
-    <main className='flex min-h-screen flex-col items-center justify-center text-white-100 bg-gray-800'>
-      <div className='flex flex-col items-center'>
-        <h1 className='mb-7 text-4xl font-bold'>Spotify Profile</h1>
-        <Link
-          href='/api/spotify_login'
-          className='px-10 py-4 rounded-full text-base font-normal tracking-[0.15em] bg-green-600 transition-all hover:bg-green-700 hover:text-white-200 hover:duration-300'
-        >
-          LOG IN TO SPOTIFY
-        </Link>
-      </div>
-    </main>
+    <div className='w-screen'>
+      {data?.user?.accessToken ? <Profile data={data} /> : <LoginPrompt />}
+    </div>
   );
 };
 
