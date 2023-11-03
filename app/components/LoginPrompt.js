8aa24479583d2d2baaa9c0d9ba244be3be9235ac
Jackie Wong <wongzjackie@gmail.com>
2023-11-02 21:01:21 -1000

    Create LoginPrompt.js
    - Create log in page that redirects to Spotify sign-in provider.

---
+++ app/components/LoginPrompt.js
@@ -0,0 +1,19 @@
+import { signIn } from 'next-auth/react';
+
+const LoginPrompt = () => {
+  return (
+    <main className='flex min-h-screen flex-col items-center justify-center text-white-100 bg-gray-800'>
+      <div className='flex flex-col items-center'>
+        <h1 className='mb-7 text-4xl font-bold'>Spotify Profile</h1>
+        <button
+          onClick={() => signIn('spotify')}
+          className='px-10 py-4 rounded-full text-base font-normal tracking-[0.15em] bg-green-600 transition-all hover:bg-green-700 hover:text-white-200 hover:duration-300'
+        >
+          LOG IN TO SPOTIFY
+        </button>
+      </div>
+    </main>
+  );
+};
+
+export default LoginPrompt;
