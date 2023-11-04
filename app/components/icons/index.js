1ea2b15157f779336a71ead0eb4bf393235b1d05
Jackie Wong <wongzjackie@gmail.com>
2023-11-04 00:49:08 -1000

    Add icons and export as components.

---
+++ app/components/icons/index.js
@@ -0,0 +1,19 @@
+import IconUser from './user';
+import IconGithub from './github';
+import IconExternal from './external';
+import IconTime from './time';
+import IconMicrophone from './microphone';
+import IconPlaylist from './playlist';
+import IconMusic from './music';
+import IconInfo from './info';
+
+export {
+  IconUser,
+  IconGithub,
+  IconExternal,
+  IconTime,
+  IconMicrophone,
+  IconPlaylist,
+  IconMusic,
+  IconInfo,
+};
