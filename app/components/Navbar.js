602e0294254e2473db92bc1c9c709d5f1cc6eac5
Jackie Wong <wongzjackie@gmail.com>
2023-11-07 02:17:29 -1000

    Add styling and functionality to Navbar.

--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -1,8 +1,16 @@
+'use client';
+import { useState } from 'react';
+import { signOut } from 'next-auth/react';
+// Components
 import Link from 'next/link';
 import Image from 'next/image';
+import { IconExternal } from './icons';
 
 const Navbar = ({ user }) => {
+  const [toggleDropdown, setToggleDropdown] = useState();
   const smallUserImg = user.images[0];
+  const account_url =
+    'https://www.spotify.com/us/account/overview/?utm_source=spotify&utm_medium=menu&utm_campaign=your_account';
 
   return (
     <nav className='bg-gray-500/0 w-2/6 h-16 absolute top-0 right-0 mt-4'>
@@ -17,14 +25,41 @@ const Navbar = ({ user }) => {
             Install App
           </Link>
         </li>
-        <li className='bg-gray-500/30 rounded-full p-1.5'>
-          <Image
-            src={smallUserImg.url}
-            width={25}
-            height={25}
-            alt='Nav profile picture'
-            className='rounded-full'
-          />
+        <li>
+          <button
+            className='bg-gray-500/30 rounded-full p-1.5'
+            onClick={() => setToggleDropdown(!toggleDropdown)}
+          >
+            <Image
+              src={smallUserImg.url}
+              width={25}
+              height={25}
+              alt='Nav profile picture'
+              className='rounded-full'
+            />
+          </button>
+          {toggleDropdown && (
+            <div className='bg-gray-500/30 rounded-md shadow-lg shadow-black absolute right-0 float-right p-1 mt-3 mr-5 text-left text-sm z-[1]'>
+              <Link
+                href={account_url}
+                target='_blank'
+                className='block py-3 pl-3 pr-3 rounded-md w-full hover:bg-gray-800 hover:transition-all ease-in-out duration-100'
+              >
+                <div className='flex justify-between items-center fill-white-100'>
+                  <p>Account</p>
+                  <IconExternal />
+                </div>
+              </Link>
+              <button
+                className='py-3 pl-3 pr-20 rounded-md hover:bg-gray-800 hover:transition-all ease-in-out duration-100'
+                onClick={() =>
+                  signOut({ callbackUrl: 'http://localhost:3000/' })
+                }
+              >
+                Sign Out
+              </button>
+            </div>
+          )}
         </li>
       </ul>
     </nav>
