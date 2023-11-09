2c017d448a9d58b4ca898e7254da59260a1e7cce
Jackie Wong <wongzjackie@gmail.com>
2023-11-08 16:07:17 -1000

    Create PlaylistCard.js
    - Refactor code in Profile.js file by creating PlaylistCard component for the playlists carousel.

---
+++ app/components/PlaylistCard.js
@@ -0,0 +1,21 @@
+import { motion } from 'framer-motion';
+import Image from 'next/image';
+
+const PlaylistCard = ({ playlist }) => {
+  return (
+    <motion.div className='bg-black p-5 rounded-md'>
+      <Image
+        src={playlist.images[0].url}
+        width={300}
+        height={300}
+        alt='Playlist image'
+      />
+      <div id='playlist-card-info' className='mt-3 mb-3'>
+        <h5 className='text-lg'>{playlist.name}</h5>
+        <h6 className='text-gray-500 text-sm'>{playlist.owner.display_name}</h6>
+      </div>
+    </motion.div>
+  );
+};
+
+export default PlaylistCard;
