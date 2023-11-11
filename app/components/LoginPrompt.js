81d46fd7f4400351a552effddb2e1667f4704052
Jackie Wong <wongzjackie@gmail.com>
2023-11-10 20:19:54 -1000

    Update LoginPrompt.js
    - Add callback URL to "signIn" function call to redirect to "/profile" route after sign in.

--- app/components/LoginPrompt.js
+++ app/components/LoginPrompt.js
@@ -6,7 +6,7 @@ const LoginPrompt = () => {
       <div className='flex flex-col items-center'>
         <h1 className='mb-7 text-4xl font-bold'>Spotify Profile</h1>
         <button
-          onClick={() => signIn('spotify')}
+          onClick={() => signIn('spotify', { callbackUrl: '/profile' })}
           className='px-10 py-4 rounded-full text-base font-normal tracking-[0.15em] bg-green-600 transition-all hover:bg-green-700 hover:text-white-200 hover:duration-300'
         >
           LOG IN TO SPOTIFY
