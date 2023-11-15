16b29587459df6f882142f7795a1530c645fd912
Jackie Wong <wongzjackie@gmail.com>
2023-11-14 15:36:51 -1000

    Update User.js
    - Remove extra "div" element surrounding top profile stats section.
    - Remove template literal for user's display name (it was unnecessary).

--- app/components/User.js
+++ app/components/User.js
@@ -41,47 +41,47 @@ const User = () => {
       className='flex flex-col items-start min-h-screen w-full text-white-100 bg-gray-800'
     >
       {user && (
-        <div>
-          <div className='flex items-center h-[30vh] w-full mb-28 pt-52 pl-44'>
-            {userImg && (
-              <div className='flex justify-start'>
-                <Image
-                  src={userImg.url}
-                  width={userImg.width}
-                  height={userImg.height}
-                  alt='Profile picture'
-                  className='rounded-full'
-                />
+        <div className='flex items-center h-[30vh] w-full mb-28 pt-52 pl-44'>
+          {userImg && (
+            <div className='flex justify-start'>
+              <Image
+                src={userImg.url}
+                width={userImg.width}
+                height={userImg.height}
+                alt='Profile picture'
+                className='rounded-full'
+              />
+            </div>
+          )}
+          <div className='flex flex-col justify-start pl-16'>
+            <p className='text-sm'>Profile</p>
+            <h1 className='text-8xl font-bold tracking-tighter'>
+              {user.display_name}
+            </h1>
+            <div className='flex justify-start items-center mt-5'>
+              <div className='flex flex-col items-center justify-center'>
+                <h2 className='text-2xl text-green-600 font-semibold'>
+                  {user.followers.total}
+                </h2>
+                <h4 className='text-gray-500 font-normal text-sm tracking-wide'>
+                  Followers
+                </h4>
               </div>
-            )}
-            <div className='flex flex-col justify-start pl-16'>
-              <p className='text-sm'>Profile</p>
-              <h1 className='text-8xl font-bold tracking-tighter'>{`${user.display_name}`}</h1>
-              <div className='flex justify-start items-center mt-5'>
-                <div className='flex flex-col items-center justify-center'>
-                  <h2 className='text-2xl text-green-600 font-semibold'>
-                    {user.followers.total}
-                  </h2>
-                  <h4 className='text-gray-500 font-normal text-sm tracking-wide'>
-                    Followers
-                  </h4>
-                </div>
-                <div className='flex flex-col items-center justify-center ml-12'>
-                  <h2 className='text-2xl text-green-600 font-semibold'>
-                    {followedArtists.artists.total}
-                  </h2>
-                  <h4 className='text-gray-500 font-normal text-sm tracking-wide'>
-                    Following
-                  </h4>
-                </div>
-                <div className='flex flex-col items-center justify-center ml-12'>
-                  <h2 className='text-2xl text-green-600 font-semibold'>
-                    {playlists.total}
-                  </h2>
-                  <h4 className='text-gray-500 font-normal text-sm tracking-wide'>
-                    Playlists
-                  </h4>
-                </div>
+              <div className='flex flex-col items-center justify-center ml-12'>
+                <h2 className='text-2xl text-green-600 font-semibold'>
+                  {followedArtists.artists.total}
+                </h2>
+                <h4 className='text-gray-500 font-normal text-sm tracking-wide'>
+                  Following
+                </h4>
+              </div>
+              <div className='flex flex-col items-center justify-center ml-12'>
+                <h2 className='text-2xl text-green-600 font-semibold'>
+                  {playlists.total}
+                </h2>
+                <h4 className='text-gray-500 font-normal text-sm tracking-wide'>
+                  Playlists
+                </h4>
               </div>
             </div>
           </div>
