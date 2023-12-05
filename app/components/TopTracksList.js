2da7f672014668621d82c0613d87edd13b9ef143
Jackie Wong <wongzjackie@gmail.com>
2023-12-04 18:13:14 -1000

    Create top tracks page.
    - Create TopTracksList component and import in top_tracks route.
    - Reuse TrackCard component.

---
+++ app/components/TopTracksList.js
@@ -0,0 +1,41 @@
+'use client';
+import { useState, useEffect } from 'react';
+import { useSession } from 'next-auth/react';
+import { fetchTopTracksOnly, catchErrors } from '@/components/utils/profile';
+// Components
+import TrackCard from './TrackCard';
+
+const TopTracksList = () => {
+  const { data } = useSession();
+  const [trackList, setTrackList] = useState();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const { tracks } = await fetchTopTracksOnly(data);
+      setTrackList(tracks);
+      console.log(tracks);
+    };
+    catchErrors(fetchData());
+  }, [data]);
+
+  return (
+    <div className='flex flex-col pt-32 pl-44'>
+      <h2 className='text-3xl mb-8'>Your Top Tracks</h2>
+      {trackList && (
+        <div className='flex flex-col justify-center mb-20'>
+          {trackList.items.map(track => {
+            return (
+              <TrackCard
+                track={track}
+                externalUrl={track.external_urls.spotify}
+                key={track.id}
+              />
+            );
+          })}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default TopTracksList;
